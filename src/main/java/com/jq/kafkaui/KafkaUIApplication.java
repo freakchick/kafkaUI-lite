a259a92f842a1f4b733a229714e4aacf091977a1
jiangqiang <jiangqiang110@126.com>
2020-10-26 18:08:45 +0800

    1

---
+++ src/main/java/com/jq/kafkaui/KafkaUIApplication.java
@@ -0,0 +1,17 @@
+package com.jq.kafkaui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: kafkaUI
+ * @description:
+ * @author: jiangqiang
+ * @create: 2020-10-26 18:03
+ **/
+@SpringBootApplication
+public class KafkaUIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaUIApplication.class, args);
+    }
+}
