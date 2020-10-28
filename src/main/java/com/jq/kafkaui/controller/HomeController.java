082313d85afe71158a51559601431520d4016b5d
jiangqiang <jiangqiang110@126.com>
2020-10-28 20:28:48 +0800

    1

---
+++ src/main/java/com/jq/kafkaui/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jq.kafkaui.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: kafkaUI
+ * @description:
+ * @author: jiangqiang
+ * @create: 2020-10-28 20:06
+ **/
+@RestController
+public class HomeController {
+}
---
+++ src/main/java/com/jq/kafkaui/util/KafkaUtil.java
@@ -0,0 +1,104 @@
+package com.jq.kafkaui.util;
+
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.config.ConfigResource;
+
+import java.util.*;
+
+/**
+ * @program: kafkaUI
+ * @description:
+ * @author: jiangqiang
+ * @create: 2020-10-28 20:05
+ **/
+public class KafkaUtil {
+
+
+    public static AdminClient createAdminClientByProperties() {
+
+        Properties prop = new Properties();
+
+        // 配置Kafka服务的访问地址及端口号
+        prop.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.33.201:9092");
+
+        // 创建AdminClient实例
+        return AdminClient.create(prop);
+    }
+
+
+    public static void listTopicsWithOptions() throws Exception {
+        // 创建AdminClient客户端对象
+        AdminClient adminClient = createAdminClientByProperties();
+
+        ListTopicsOptions options = new ListTopicsOptions();
+        // 列出内部的Topic
+        options.listInternal(true);
+
+        // 列出所有的topic
+        ListTopicsResult result = adminClient.listTopics(options);
+
+        // 获取所有topic的名字，返回的是一个Set集合
+        Set<String> topicNames = result.names().get();
+
+        topicNames.forEach(System.out::println);
+
+        // 获取所有topic的信息，返回的是一个Collection集合
+        // (name=hello-kafka, internal=false),internal代表是否为内部的topic
+        Collection<TopicListing> topicListings = result.listings().get();
+
+        // 打印所有topic的信息
+        topicListings.forEach(System.out::println);
+
+        // 关闭资源
+        adminClient.close();
+    }
+
+    /**
+     * 获取topic的描述信息
+     *
+     * topic name = a-topic, desc = (name=a-topic, internal=false, partitions=(partition=0, leader=192.168.182.128:9092 (id: 0 rack: null), replicas=192.168.182.128:9092 (id: 0 rack: null), isr=192.168.182.128:9092 (id: 0 rack: null)), authorizedOperations=null)
+     * topic name = b-topic, desc = (name=b-topic, internal=false, partitions=(partition=0, leader=192.168.182.128:9092 (id: 0 rack: null), replicas=192.168.182.128:9092 (id: 0 rack: null), isr=192.168.182.128:9092 (id: 0 rack: null)), authorizedOperations=null)
+     */
+    public static void describeTopics(List<String> topics) throws Exception {
+        // 创建AdminClient客户端对象
+        AdminClient adminClient = createAdminClientByProperties();
+
+        // 获取Topic的描述信息
+        DescribeTopicsResult result = adminClient.describeTopics(topics);
+
+        // 解析描述信息结果, Map<String, TopicDescription> ==> topicName:topicDescription
+        Map<String, TopicDescription> topicDescriptionMap = result.all().get();
+        topicDescriptionMap.forEach((topicName, description) -> System.out.printf("topic name = %s, desc = %s \n", topicName, description));
+
+        // 关闭资源
+        adminClient.close();
+    }
+
+    /**
+     * 获取topic的配置信息
+     */
+    public static void describeConfigTopics(List<String> topicNames) throws Exception {
+        // 创建AdminClient客户端对象
+        AdminClient adminClient = createAdminClientByProperties();
+
+        List<ConfigResource> configResources = new ArrayList<>();
+        topicNames.forEach(topicName -> configResources.add(
+                // 指定要获取的源
+                new ConfigResource(ConfigResource.Type.TOPIC, topicName)));
+
+        // 获取topic的配置信息
+        DescribeConfigsResult result = adminClient.describeConfigs(configResources);
+
+        // 解析topic的配置信息
+        Map<ConfigResource, Config> resourceConfigMap = result.all().get();
+        resourceConfigMap.forEach((configResource, config) -> System.out.printf("topic config ConfigResource = %s, Config = %s \n", configResource, config));
+
+        // 关闭资源
+        adminClient.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        listTopicsWithOptions();
+    }
+
+}
