be7d070090c510442ebc484e92193428f75078e9
jiangqiang <jiangqiang110@126.com>
2020-10-29 10:47:12 +0800

    2

--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -8,11 +8,14 @@ Vue.config.productionTip = false
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+//使用vue-axios，这样才可以全局使用this.axios调用
 Vue.use(VueAxios, axios);
 import qs from 'qs'
+
 axios.defaults.baseURL = '/api'
 
 axios.defaults.headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+//全局拦截post请求的参数，用qs序列化
 axios.interceptors.request.use(config => {
   //form表单提交multipart/form-data的时候，不需要序列化参数
   if(config.method === 'post' && config.headers['Content-Type'] === 'application/x-www-form-urlencoded'){
--- src/main/webapp/vue.config.js
+++ src/main/webapp/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     devServer: {
         proxy: "http://127.0.0.1:6666", //开发环境的跨域问题解决
-        port: 8085
+        port: 6669
     }
 }
