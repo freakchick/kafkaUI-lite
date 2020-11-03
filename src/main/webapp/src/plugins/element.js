217d3056035e504dba9cf8791b36fe2ad672392b
jiangqiang <jiangqiang110@126.com>
2020-11-03 11:10:05 +0800

    2

--- src/main/webapp/src/plugins/element.js
+++ src/main/webapp/src/plugins/element.js
@@ -2,25 +2,25 @@ import Vue from 'vue'
 // import { Button,ELCONTAINER } from 'element-ui'
 
 import {
-    Pagination,
+    // Pagination,
     Dialog,
-    Autocomplete,
-    Dropdown,
-    DropdownMenu,
-    DropdownItem,
-    Menu,
-    Submenu,
-    MenuItem,
-    MenuItemGroup,
+    // Autocomplete,
+    // Dropdown,
+    // DropdownMenu,
+    // DropdownItem,
+    // Menu,
+    // Submenu,
+    // MenuItem,
+    // MenuItemGroup,
     Input,
     InputNumber,
     Radio,
     RadioGroup,
     RadioButton,
     Checkbox,
-    CheckboxButton,
-    CheckboxGroup,
-    Switch,
+    // CheckboxButton,
+    // CheckboxGroup,
+    // Switch,
     Select,
     Option,
     OptionGroup,
@@ -28,61 +28,62 @@ import {
     ButtonGroup,
     Table,
     TableColumn,
-    DatePicker,
-    TimeSelect,
-    TimePicker,
-    Popover,
+    // DatePicker,
+    // TimeSelect,
+    // TimePicker,
+    // Popover,
     Tooltip,
-    Breadcrumb,
-    BreadcrumbItem,
+    // Breadcrumb,
+    // BreadcrumbItem,
     Form,
     FormItem,
-    Tabs,
-    TabPane,
-    Tag,
-    Tree,
-    Alert,
-    Slider,
-    Icon,
-    Row,
-    Col,
-    Upload,
-    Progress,
-    Spinner,
-    Badge,
-    Card,
-    Rate,
-    Steps,
-    Step,
-    Carousel,
-    CarouselItem,
-    Collapse,
-    CollapseItem,
-    Cascader,
-    ColorPicker,
-    Transfer,
+    // Tabs,
+    // TabPane,
+    // Tag,
+    // Tree,
+    // Alert,
+    // Slider,
+    // Icon,
+    // Row,
+    // Col,
+    // Upload,
+    // Progress,
+    // Spinner,
+    // Badge,
+    // Card,
+    // Rate,
+    // Steps,
+    // Step,
+    // Carousel,
+    // CarouselItem,
+    // Collapse,
+    // CollapseItem,
+    // Cascader,
+    // ColorPicker,
+    // Transfer,
     Container,
     Header,
     Aside,
     Main,
     Footer,
-    Timeline,
-    TimelineItem,
-    Link,
-    Divider,
-    Image,
-    Calendar,
-    Backtop,
-    PageHeader,
-    CascaderPanel,
-    Loading,
-    MessageBox,
+    // Timeline,
+    // TimelineItem,
+    // Link,
+    // Divider,
+    // Image,
+    // Calendar,
+    // Backtop,
+    // PageHeader,
+    // CascaderPanel,
+    // Loading,
+    // MessageBox,
     Message,
-    Notification
+    // Notification
 } from 'element-ui';
 
-Vue.use(Pagination);
+// Vue.use(Pagination);
 Vue.use(Dialog);
+/*
 Vue.use(Autocomplete);
 Vue.use(Dropdown);
 Vue.use(DropdownMenu);
@@ -90,16 +91,19 @@ Vue.use(DropdownItem);
 Vue.use(Menu);
 Vue.use(Submenu);
 Vue.use(MenuItem);
-Vue.use(MenuItemGroup);
+Vue.use(MenuItemGroup);*/
 Vue.use(Input);
 Vue.use(InputNumber);
 Vue.use(Radio);
 Vue.use(RadioGroup);
 Vue.use(RadioButton);
 Vue.use(Checkbox);
+/*
 Vue.use(CheckboxButton);
 Vue.use(CheckboxGroup);
 Vue.use(Switch);
+*/
+
 Vue.use(Select);
 Vue.use(Option);
 Vue.use(OptionGroup);
@@ -107,15 +111,22 @@ Vue.use(Button);
 Vue.use(ButtonGroup);
 Vue.use(Table);
 Vue.use(TableColumn);
+/*
 Vue.use(DatePicker);
 Vue.use(TimeSelect);
 Vue.use(TimePicker);
 Vue.use(Popover);
+*/
+
 Vue.use(Tooltip);
+/*
 Vue.use(Breadcrumb);
 Vue.use(BreadcrumbItem);
+*/
+
 Vue.use(Form);
 Vue.use(FormItem);
+/*
 Vue.use(Tabs);
 Vue.use(TabPane);
 Vue.use(Tag);
@@ -139,12 +150,13 @@ Vue.use(Collapse);
 Vue.use(CollapseItem);
 Vue.use(Cascader);
 Vue.use(ColorPicker);
-Vue.use(Transfer);
+Vue.use(Transfer);*/
 Vue.use(Container);
 Vue.use(Header);
 Vue.use(Aside);
 Vue.use(Main);
 Vue.use(Footer);
+/*
 Vue.use(Timeline);
 Vue.use(TimelineItem);
 Vue.use(Link);
@@ -156,7 +168,7 @@ Vue.use(PageHeader);
 Vue.use(CascaderPanel);
 
 Vue.use(Loading.directive);
-
+*/
 
 Vue.prototype.$message = Message
 // Vue.use(Button)
